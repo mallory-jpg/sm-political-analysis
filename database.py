e77925e1fd1f64f3e1aedd98e008d3792a5b3c6d
mallory-jpg <65197541+mallory-jpg@users.noreply.github.com>
2021-08-26 16:24:43 -0500

    update dependencies

--- database.py
+++ database.py
@@ -6,19 +6,124 @@ from psycopg2 import Error
 import configparser
 from timer import Timer
 
+# create db
+create_database_query = """
+        CREATE DATABASE IF NOT EXISTS sm_news;
+    """
+# create necessary tables
+create_article_table = """
+    CREATE TABLE IF NOT EXISTS articles (
+        publishedAt DATE,
+        title VARCHAR PRIMARY KEY,
+        author VARCHAR,
+        url TEXT
+        );
+    """
+create_article_table_index = """
+    CREATE INDEX index
+        ON articles(publishedAt,
+            title
+        );
+    """
+create_article_text_table = """
+    CREATE TABLE IF NOT EXISTS article_text (
+        title VARCHAR PRIMARY KEY,
+        article_text TEXT
+        );
+    """
+create_political_event_table = """
+    CREATE TABLE IF NOT EXISTS event (
+        eventID ID PRIMARY KEY,
+        startDate DATE,
+        name VARCHAR NOT NULL,
+        description VARCHAR NOT NULL,
+        keyWords VARCHAR
+        );
+ """
+create_tweets_table = """
+    CREATE TABLE IF NOT EXISTS tweets (
+        tweet_id INT PRIMARY KEY,
+        publishedAt DATE NOT NULL,
+        userID VARCHAR NOT NULL,
+        tweet VARCHAR NOT NULL,
+        location VARCHAR NOT NULL,
+        tags VARCHAR NOT NULL
+        );
+    """
+create_tiktoks_table = """
+    CREATE TABLE IF NOT EXISTS tiktoks (
+        postID INT PRIMARY KEY,
+        createTime DATE NOT NULL,
+        userID INT NOT NULL,
+        description VARCHAR NOT NULL,
+        musicID INT NOT NULL,
+        soundID INT NOT NULL,
+        tags VARCHAR NOT NULL
+        );
+    """
+create_tiktok_sounds_table = """
+    CREATE TABLE IF NOT EXISTS tiktok_sounds (
+        soundID INT PRIMARY KEY,
+        soundTitle VARCHAR,
+        isOriginal BOOLEAN
+        );
+    """
+create_tiktok_music_table = """
+    CREATE TABLE IF NOT EXISTS tiktok_music (
+        songID INT PRIMARY KEY,
+        songTitle VARCHAR NOT NULL
+        );
+    """
+
+create_tiktok_stats_table = """
+    CREATE TABLE IF NOT EXISTS tiktok_stats (
+        postID INT PRIMARY KEY,
+        shareCount INT,
+        commentCount INT,
+        playCount INT,
+        diggCount INT
+        );
+    """
+
+create_tiktok_tags_table = """
+    CREATE TABLE IF NOT EXISTS tiktok_tags (
+        tagID INT PRIMARY KEY,
+        tag_name VARCHAR NOT NULL
+        );
+    """
+create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+        userID INT PRIMARY KEY,
+        username VARCHAR NOT NULL,
+        user_bio VARCHAR NOT NULL
+        );
+    """
+delete_bad_data = """
+    DELETE FROM articles
+        WHERE publishedAt IS NULL;
+    """
+
+# add foreign keys
+alter_tiktoks_table = """
+    ALTER TABLE tiktoks
+    ADD FOREIGN KEY(musicID) REFERENCES tiktok_music(songID),
+    ADD FOREIGN KEY(soundID) REFERENCES tiktok_sounds(soundID),
+    ADD FOREIGN KEY(userID) REFERENCES users(userID)
+    ON DELETE SET NULL;
+"""
+alter_tiktok_stats_table = """
+    ALTER TABLE tiktok_stats
+    ADD FOREIGN KEY(postID) REFERENCES tiktoks(postID)
+    ON DELETE SET NULL;
+"""
 
 
 class DataBase():
-    
-    def __init__(self, host_name, user_name, user_password, logger=logging):
+    def __init__(self, host_name, user_name, user_password):
         self.host_name = host_name
         self.user_name = user_name
         self.user_password = user_password
-        # self.logger = logging.basicConfig(filename='db.log', filemode='w',
-        #                                               format=f'%(asctime)s - %(levelname)s - %(message)s')
 
-        self.logger = logging.getLogger(__name__)
-    @Timer("Server cxn")
     def create_server_connection(self):
         self.connection = None
         try:
@@ -33,45 +138,43 @@ class DataBase():
 
         return self.connection
 
-
     def create_database(self, connection, query):
-            self.connection = connection
-            cursor = connection.cursor()
-            try:
+        self.connection = connection
+        cursor = connection.cursor()
+        try:
                 cursor.execute(query)
                 logging.info("Database created successfully")
-            except Error as err:
+        except Error as err:
                 logging.error(f"Error: '{err}'")
 
-    @Timer("Database cxn")
     def create_db_connection(self, db_name):
-            self.db_name = db_name
-            self.connection = None
-            try:
-                connection = psycopg2.connect(
+        self.db_name = db_name
+        self.connection = None
+        try:
+                self.connection = psycopg2.connect(
                     host=self.host_name,
                     user=self.user_name,
                     password=self.user_password,
                     database=self.db_name
                 )
                 # cursor = connection.cursor()
-                logging.info("Database connection successful")
-            except Error as err:
+                logging.info("PostgreSQL Database connection successful")
+        except Error as err:
                 logging.error(f"Error: '{err}'")
 
-            return self.connection
+        return self.connection
 
-    @Timer(name='Query Execution')
+    @Timer(name='Query Execution')  # *TODO fix __enter__ attribute error
     def execute_query(self, connection, query):
-            self.connection = connection
-            cursor = self.connection.cursor()
-            try:
+        self.connection = connection
+        cursor = connection.cursor()
+        try:
                 cursor.execute(query)
                 self.connection.commit()
                 logging.info("Query successful")
-            except Error as err:
+        except Error as err:
                 print(f"Error: '{err}'")
-    
+
     def read_query(self, connection, query):
         self.connection = connection
         cursor = self.connection.cursor()
@@ -92,14 +195,14 @@ class DataBase():
         self.connection = conn
         # Create a list of tupples from the dataframe values
         tuples = [tuple(x) for x in df.to_numpy()]
-    
+
         # Comma-separated dataframe columns
         cols = ','.join(list(df.columns))
-    
+
         # SQL query to execute
         cursor = conn.cursor()
         values = [cursor.mogrify("(%s,%s,%s,%s)", tup).decode('utf8')
-                for tup in tuples]
+                  for tup in tuples]
         # if not publishedAt, delete record
         query = "INSERT INTO %s(%s) VALUES" % (table, cols) + ",".join(values)
 
@@ -117,127 +220,3 @@ class DataBase():
         cursor.close()
         conn.close()
 
-# DDL queries
-create_database_query = """
-        CREATE DATABASE IF NOT EXISTS sm_news; 
-    """
-    # create necessary tables
-    # keyWords VARCHAR
-create_article_table = """
-    CREATE TABLE IF NOT EXISTS articles (
-        publishedAt DATE,
-        title VARCHAR PRIMARY KEY,
-        author VARCHAR,
-        url TEXT
-        );
-    """    
-create_article_table_index = """
-    CREATE INDEX index 
-        ON articles(publishedAt, 
-            title
-        );
-    """
-create_article_text_table = """
-    CREATE TABLE IF NOT EXISTS article_text (
-        title VARCHAR PRIMARY KEY REFERENCES articles (title),
-        text
-        );
-    """
-create_article_text_table_index = """
-    CREATE INDEX index 
-        ON article_text(publishedAt, title
-        );
-    """
-    # CREATE INDEX index ON articles(publishedAt);
-create_political_event_table = """
-    CREATE TABLE IF NOT EXISTS event (
-        eventID VARCHAR PRIMARY KEY,
-        startDate DATE,
-        name VARCHAR NOT NULL,
-        description VARCHAR NOT NULL,
-        keyWords VARCHAR
-        );
-    """
-    # TODO clean df tables so they match 
-create_batch_tweets_table = """
-    CREATE TABLE IF NOT EXISTS batch_tweets (
-        tweet_id INT PRIMARY KEY,
-        created_at DATE NOT NULL,
-        userID INT NOT NULL,
-        tweet VARCHAR NOT NULL,
-        location VARCHAR NOT NULL, 
-        tags VARCHAR NOT NULL
-        );
-    """
-create_stream_tweets_table = """
-    CREATE TABLE IF NOT EXISTS stream_tweets (
-        tweet_id INT PRIMARY KEY,
-        publishedAt DATE NOT NULL,
-        userID INT NOT NULL,
-        tweet VARCHAR NOT NULL,
-        location VARCHAR NOT NULL, 
-        tags VARCHAR NOT NULL
-        );
-    """
-create_tweet_trends_table = """
-    CREATE TABLE IF NOT EXISTS tweets_trends (
-        tweet_id INT PRIMARY KEY,
-        publishedAt DATE NOT NULL,
-        userID INT NOT NULL,
-        tweet VARCHAR NOT NULL,
-        location VARCHAR NOT NULL, 
-        tags VARCHAR NOT NULL
-        );
-    """
-create_tiktoks_table = """
-    CREATE TABLE IF NOT EXISTS tiktoks (
-        postID INT PRIMARY KEY,
-        createTime DATE NOT NULL,
-        description VARCHAR NOT NULL,
-        musicID VARCHAR NOT NULL,
-        tags VARCHAR NOT NULL,
-        FOREIGN KEY(songID) REFERENCES tiktok_music(songID),
-        FOREIGN KEY(soundID) REFERENCES tiktok_sounds(soundID),
-        FOREIGN KEY(userID) REFERENCES users(userID)
-        );
-    """
-create_tiktok_sounds_table = """
-    CREATE TABLE IF NOT EXISTS tiktok_sounds (
-        soundID INT PRIMARY KEY,
-        soundTitle VARCHAR,
-        isOriginal BOOLEAN
-        );
-    """
-create_tiktok_music_table = """
-    CREATE TABLE IF NOT EXISTS tiktok_music (
-        songID INT PRIMARY KEY,
-        songTitle VARCHAR NOT NULL
-        );
-    """
-create_tiktok_stats_table = """
-    CREATE TABLE IF NOT EXISTS tiktok_stats (
-        FOREIGN KEY(postID) REFERENCES tiktoks(postID),
-        shareCount INT,
-        commentCount INT,
-        playCount INT,
-        diggCount INT
-        );
-    """
-
-create_tiktok_tags_table = """
-    CREATE TABLE IF NOT EXISTS tiktok_tags (
-        tagID INT PRIMARY KEY,
-        tag_name VARCHAR NOT NULL 
-        );
-    """
-create_users_table = """
-    CREATE TABLE IF NOT EXISTS users (
-        userID INT PRIMARY KEY,
-        username VARCHAR NOT NULL,
-        user_bio VARCHAR NOT NULL
-        );
-    """
-delete_bad_data = """
-    DELETE FROM articles
-        WHERE publishedAt IS NULL;
-    """
--- news.py
+++ news.py
@@ -1,62 +1,256 @@
 """Compute weight of words in articles and titles"""
 
-from django.db import DatabaseError
+# from django.db import DatabaseError
 from get_news import *
 import requests
 from bs4 import BeautifulSoup
 from database import *
 import logging
-from keywords import *
+import re
+from nltk import sent_tokenize, tokenize
+import nltk
+# from keywords import *
 
-# https://towardsdatascience.com/tf-idf-for-document-ranking-from-scratch-in-python-on-real-world-dataset-796d339a4089 
+# make sure .txt of user agents is in directory
+def get_random_ua(browser):
+    random_ua = ''
+    ua_file = f'{browser}.txt'.title()
 
-#create dataframe?
-article_text_df = pd.DataFrame({'index': '',
-                             'title': '',
-                             'text': ''
-                            })
+    try:
+        with open(ua_file) as f:
+            lines = f.readlines()
+        if len(lines) > 0:
+            prng = np.random.RandomState()
+            index = prng.permutation(len(lines) - 1)
+            idx = np.asarray(index, dtype=np.integer)[0]
+            random_proxy = lines[int(idx)]
+    except Exception as ex:
+        logging('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua
 
-# execute query to create corresponding table in db
-connection = create_db_connection(c['database']['host'],
-                                  c['database']['user'],
-                                  c['database']['password'],
-                                  c['database']['database'])
-try:
-    execute_query(connection, create_article_text_table)
-except (DatabaseError, ConnectionError) as e:
-    logging.error({e}, ': please check article_text table and functions ')
-else:
-    execute_mogrify(connection, article_text_df, 'article_text')
+class News():
+    """Extract keywords from  news articles to use as search values for TikTok & Twitter posts relating to the political event of interest. """
 
+    def __init__(self, api_key, logger=logging):
+        self.api_key = api_key
+        self.logger = logging.basicConfig(filename='news.log', filemode='w',
+                                          format=f'%(asctime)s - %(levelname)s - %(message)s')
 
-def get_text(url): 
-    """Get article text using Requests and BeautifulSoup"""
-    r = requests.get(url)
-    html = r.text
-    soup = BeautifulSoup(html)
-    soup.get_text()
+    def request_pop_news(self, params={
+        'q': ['politics' or 'political' or 'law' or 'legal' or 'policy'],
+        'from': {date.today() - timedelta(days=3)},
+        'to': {date.today},
+        'language': 'en',
+        'sort_by': 'popularity'
+    }):
+        pop_news = []
+        self.params = params
 
+        headers = {
+            'X-Api-Key': self.api_key,
+            # get_random_ua for Chrome
+            'user-agent': get_random_ua('Chrome')
+        }
 
-#* CREATE A KEY BASED ON THE WORD MASH?
-# apply get_text function using urls from all_news df
-url_text = all_news['url'].apply(
-    lambda row: get_text(all_news['url']),
-    axis=1
-)
+        url = 'https://newsapi.org/v2/everything'
 
-# put text into df
-article_text_df['text'] = url_text
+        # response as JSON dict
+        self.response = requests.get(
+            url, params=self.params, headers=headers).json()
 
-#* extract keywords from text to new df column
-# article_text_df['keys'] = keyword_extraction(article_text)
-keywords = article_text_df['keywords'].apply(
-    lambda row: get_top_n(tf_idf_score, 3) # looks like i'm gonna have to create a class ughhhh
-)
+        with open('pop_news.json', 'w') as f:
+            # write results to JSON file
+            json.dump(self.response, f)
 
-# append text and keys to database
-execute_mogrify(connection, article_text_df, 'article_text')
+        with open('pop_news.json', 'r') as file:
+            # create Python list object from JSON
+            pop_news_json = file.read().split("\n")
 
-# add 3 columns to df/db -> same number of keywords (3) everytime + can rank by importance w kw1 kw2 kw3 in KEYWORDS.py
-## combine columns into list/iterable 
-## combination generator -> search for any combination of the top three words on twitter and tiktok
-# decorator to make results an iterable?
+            for story in pop_news_json:
+                pop_obj = json.loads(story)
+
+                if 'title' in pop_obj:
+                    pop_obj['title'] = pop_obj['articles']['title']
+                if 'author' in pop_obj:
+                    pop_obj['author'] = pop_obj['articles']['author']
+                if 'url' in pop_obj:
+                    pop_obj['url'] = pop_obj['articles']['url']
+                if 'publishedAt' in pop_obj:
+                    pop_obj['publishedAt'] = pop_obj['articles']['publishedAt']
+
+                # add info to pop_news dict
+                pop_news.append(pop_obj)
+
+        return pop_news
+
+    def get_top_headlines(self, params={
+        "language": "en",
+        "country": "us"
+    }):
+
+        top_headlines = []
+        self.params = params
+
+        headers = {
+            "X-Api-Key": self.api_key,
+            "user-agent": get_random_ua('Chrome')
+        }
+        url = "https://newsapi.org/v2/top-headlines"
+
+        self.response = requests.get(
+            url, params=self.params, headers=headers).json()  # response JSON dict
+
+        with open("top_headlines.json", "w") as f:
+            # write results to JSON file
+            json.dump(self.response, f)
+
+        with open("top_headlines.json", "r") as file:
+            # create Python object from JSON
+            top_headlines_json = file.read().split("\n")
+
+            for story in top_headlines_json:
+                story_obj = json.loads(story)
+
+                if 'title' in story_obj:
+                    story_obj["title"] = story_obj["articles"]["title"]
+                if 'author' in story_obj:
+                    story_obj["author"] = story_obj["articles"]["author"]
+                if 'url' in story_obj:
+                    story_obj["url"] = story_obj["articles"]["url"]
+                if 'publishedAt' in story_obj:
+                    story_obj["publishedAt"] = story_obj["articles"]["publishedAt"]
+
+                # add info to top_headlines list/dict
+                top_headlines.append(story_obj)
+
+        return top_headlines
+
+    # put all news together
+    def get_all_news(self):
+        """Combines top headlines and popular news into one Pandas DataFrame."""
+        top_headlines = self.get_top_headlines()
+        pop_news = self.request_pop_news()
+
+        pop_news = pd.json_normalize(pop_news, record_path=['articles'])
+        top_headlines = pd.json_normalize(
+            top_headlines, record_path=['articles'])
+        all_news = top_headlines.append(pop_news)
+        self.all_news_df = pd.DataFrame(
+            all_news, columns=['title', 'author', 'url', 'publishedAt', "text", "keyword1", "keyword2", "keyword3"])
+        self.all_news_df.drop_duplicates()
+
+        # convert to datetime
+        self.all_news_df['publishedAt'] = self.all_news_df['publishedAt'].map(
+            lambda row: datetime.strptime(str(row), "%Y-%m-%dT%H:%M:%SZ") if pd.notnull(row) else row)
+
+        self.all_news_df.set_index('publishedAt', inplace=True)
+
+        self.all_news_df["text"] = self.all_news_df["url"].apply(
+            self.get_article_text)
+
+        return self.all_news_df
+
+    def get_article_text(self, url):
+        """Get and clean news article text"""
+
+        contractions_dict = {"'s": " is", "n't": " not", "'m": " am", "'ll": " will",
+                             "'d": " would", "'ve": " have", "'re": " are"}
+        symbols_list = ['&', '+', '-', '/', '|', '$', '%', ':']
+
+        # request
+        r = requests.get(url)
+        html = r.text
+        soup = BeautifulSoup(html)
+        a_text = soup.get_text()
+
+        # remove newline characters
+        a_text = a_text.strip()
+        # remove mentions
+        a_text = re.sub("@\S+", " ", a_text)
+        # remove URLs
+        a_text = re.sub("https*\S+", " ", a_text)
+        # remove hashtags
+        a_text = re.sub("#\S+", " ", a_text)
+        # remove unicode characters
+        a_text = a_text.encode('ascii', 'ignore').decode()
+        # replace contractions
+        for key, value in contractions_dict.items():
+            if key in a_text:
+                a_text = a_text.replace(key, value)
+
+        for i in symbols_list:
+            if i in a_text:
+                a_text = a_text.replace(i, '')
+
+        # make lowercase
+        a_text = a_text.lower()
+        a_text = re.sub(r'\w*\d+\w*', '', a_text)
+
+        return a_text
+
+    def keyword_extraction(self, text):
+        """Determine weight of important words in articles and add to articles_text table
+        using TF-IDF ranking"""
+
+        # make sure text is in string format for parsing
+        text = str(text)
+        stop_words = set(stopwords.words('english'))
+
+        # find total words in document for calculating Term Frequency (TF)
+        total_words = text.split()
+        total_word_length = len(total_words)
+
+        # find total number of sentences for calculating Inverse Document Frequency
+        total_sentences = tokenize.sent_tokenize(text)
+        total_sent_len = len(total_sentences)
+
+        # calculate TF for each word
+        tf_score = {}
+        for each_word in total_words:
+            each_word = each_word.replace('.', '')
+            if each_word not in stop_words:
+                if each_word in tf_score:
+                    tf_score[each_word] += 1
+                else:
+                    tf_score[each_word] = 1
+
+        # Divide by total_word_length for each dictionary element
+        tf_score.update((x, y/int(total_word_length))
+                        for x, y in tf_score.items())  # TODO test - ZeroError
+
+        #calculate IDF for each word
+        idf_score = {}
+        for each_word in total_words:
+            each_word = each_word.replace('.', '')
+            if each_word not in stop_words:
+                if each_word in idf_score:
+                    idf_score[each_word] = self.check_sent(
+                        each_word, total_sentences)
+                else:
+                    idf_score[each_word] = 1
+
+        # Performing a log and divide
+        idf_score.update((x, math.log(int(total_sent_len)/y))
+                         for x, y in idf_score.items())
+
+        # Calculate IDF * TF for each word
+        tf_idf_score = {key: tf_score[key] *
+                        idf_score.get(key, 0) for key in tf_score.keys()}
+
+        return tf_idf_score
+
+    def check_sent(self, word, sentences):
+        """Check if word is present in sentence list for calculating IDF (Inverse Document Frequency)"""
+        final = [all([w in x for w in word]) for x in sentences]
+        sent_len = [sentences[i] for i in range(0, len(final)) if final[i]]
+
+        return int(len(sent_len))
+
+    def get_top_n(self, dict_elem, n):
+        """Calculate most important keywords in text of interest"""
+        result = dict(sorted(dict_elem.keys(),
+                             key=itemgetter(2), reverse=True)[:n])
+
+        return result
